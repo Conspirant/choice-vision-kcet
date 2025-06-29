a73594c6038eaa3f3a0afad22447cceafa38608d
Conspirant <creepmodder185@gmail.com>
2025-06-29 17:35:43 +0530

    Temporarily remove React Query to fix build issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import { useState, useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Planner from "./pages/Planner";
 import NotFound from "./pages/NotFound";
 import AccessPopup from "./components/AccessPopup";
-
-const queryClient = new QueryClient();
 
 const App = () => {
   const [hasAcceptedTerms, setHasAcceptedTerms] = useState(false);
@@ -32,20 +29,18 @@ const App = () => {
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/planner" element={<Planner />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/planner" element={<Planner />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   );
 };
 
